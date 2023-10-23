73cb690aa04d48c68452d7eb24b925a966b57fb0
Thientitas Udomsiri <tasudomsiri@gmail.com>
2023-10-23 15:32:39 +0700

    finished setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "@/App.css";
+import { create } from "zustand";
+
+type Store = {
+  count: number;
+  inc: () => void;
+};
+
+const useStore = create<Store>()((set) => ({
+  count: 1,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+}));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count1, setCount] = useState(0);
+
+  const { count, inc } = useStore();
 
   return (
     <>
@@ -19,7 +32,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count1}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -28,8 +41,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <p>Zustand</p>
+        <div>
+          <span>{count}</span>
+          <button onClick={inc}>one up</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
